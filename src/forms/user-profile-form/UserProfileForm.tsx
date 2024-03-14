b68c6cbd9fe5c11dec7224bbc0bf21faef5dbb50
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-03-14 14:59:43 +0200

    implemented filling out the form with user data

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -13,6 +14,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { LoadingButton } from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
+import { User } from "@/common/types/user";
 
 const formSchema = z.object({
   email: z.string().optional(),
@@ -25,14 +27,20 @@ const formSchema = z.object({
 export type UserFormData = z.infer<typeof formSchema>;
 
 type Props = {
+  currentUser: User;
   onSave: (userProfileData: UserFormData) => void;
   isLoading: boolean;
 };
 
-export const UserProfileForm = ({ onSave, isLoading }: Props) => {
+export const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
+    defaultValues: currentUser,
   });
+
+  useEffect(() => {
+    form.reset(currentUser);
+  }, [currentUser, form]);
 
   return (
     <Form {...form}>
