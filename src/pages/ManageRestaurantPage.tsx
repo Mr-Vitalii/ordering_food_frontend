985e3f1e1d8e2dce7c434c6a222092c37b4c5bac
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-03-19 20:05:25 +0200

    implemented the ability to update hotel data

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,6 +1,7 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import { ManageRestaurantForm } from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
@@ -8,11 +9,15 @@ export const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isCreateLoading } =
     useCreateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
   return (
     <ManageRestaurantForm
       restaurant={restaurant}
-      onSave={createRestaurant}
-      isLoading={isCreateLoading}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
     />
   );
 };
