9f6fd45b93faceedd268d52c9fb3a1e6c8879e09
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-03-22 15:04:41 +0200

    added a query to search for a restaurant

--- src/common/types/search.ts
+++ src/common/types/search.ts
@@ -1,8 +1,18 @@
 import { SearchForm } from "@/components/SearchBar";
+import { Restaurant } from "./my-restaurant";
 
 export type SearchBarProps = {
   onSubmit: (formData: SearchForm) => void;
   placeHolder: string;
   onReset?: () => void;
   searchQuery?: string;
+};
+
+export type RestaurantSearchResponse = {
+  data: Restaurant[];
+  pagination: {
+    total: number;
+    page: number;
+    pages: number;
+  };
 };
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,22 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 export const SearchPage = () => {
   const { city } = useParams();
+  const { results, isLoading } = useSearchRestaurants(city);
 
-  return <div>User search for: {city}</div>;
+  return (
+    <div>
+      {city}
+      <ul>
+        {results?.data.map((restaurant) => (
+          <li key={uuidv4()}>
+            {restaurant.restaurantName}, {restaurant.city}
+            {/* <SearchResultCard restaurant={restaurant} /> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
