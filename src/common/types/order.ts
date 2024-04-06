2b6a7485bf423b61197e638b056a9b4bd0b75f09
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-04-06 19:45:09 +0300

    added orders request

--- src/common/types/order.ts
+++ src/common/types/order.ts
@@ -1,3 +1,6 @@
+import { Restaurant } from "./my-restaurant";
+import { User } from "./user";
+
 export type CheckoutSessionRequest = {
   cartItems: {
     menuItemId: string;
@@ -10,5 +13,33 @@ export type CheckoutSessionRequest = {
     addressLine1: string;
     city: string;
   };
+  restaurantId: string;
+};
+
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
+export type Order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    name: string;
+    addressLine1: string;
+    city: string;
+    email: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
   restaurantId: string;
 };
