e37ff99451efa10e1135a9c389d5f9af1ecd17bc
Vitalii <co4ckowoi.v@gmail.com>
2025-06-28 18:40:49 +0300

    add cities for test

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,33 @@
-import { useNavigate } from "react-router-dom";
-import landingImage from "../assets/landing.jpg";
-import appDownloadImage from "../assets/appDownload.png";
-import { SearchBar, SearchForm } from "@/components/SearchBar";
+import { useNavigate } from 'react-router-dom'
+import landingImage from '../assets/landing.jpg'
+import appDownloadImage from '../assets/appDownload.png'
+import { SearchBar, SearchForm } from '@/components/SearchBar'
 
 export const HomePage = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleSearchSubmit = (searchFormValues: SearchForm) => {
     navigate({
       pathname: `/search/${searchFormValues.searchQuery}`,
-    });
-  };
+    })
+  }
   return (
-    <div className="flex flex-col gap-12">
-      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+    <div className='flex flex-col gap-12'>
+      <div className='md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
+        <h1 className='text-5xl font-bold tracking-tight text-orange-600'>
           Tuck into a takeway today
         </h1>
-        <span className="text-xl">Food is just a click away!</span>
-        <SearchBar
-          placeHolder="Search by City or Town"
-          onSubmit={handleSearchSubmit}
-        />
+        <span className='text-xl'>Food is just a click away!</span>
+        <SearchBar placeHolder='Search by city' onSubmit={handleSearchSubmit} />
+        <span className='text-sm text-left -mt-4 ml-4 opacity-50'>
+          Cities for test check: Kyiv, Kharkov, London, Paris, Vienna, Rome,
+          Madrid.
+        </span>
       </div>
-      <div className="grid md:grid-cols-2 gap-5">
+      <div className='grid md:grid-cols-2 gap-5'>
         <img src={landingImage} />
-        <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <span className="font-bold text-3xl tracking-tighter">
+        <div className='flex flex-col items-center justify-center gap-4 text-center'>
+          <span className='font-bold text-3xl tracking-tighter'>
             Order takeaway even faster!
           </span>
           <span>
@@ -37,5 +38,5 @@ export const HomePage = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
